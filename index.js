6c30351762e09e8b1231bd812e24de51761ce941
Jake Overall <joverall22@gmail.com>
2018-05-23 12:29:10 -0600

    basic auth without roles

--- index.js
+++ index.js
@@ -11,9 +11,13 @@ require('./server-assets/db/mlab-config')
 
 //REGISTER MIDDLEWEAR
 app.use(bp.json())
-app.use(bp.urlencoded({extended: true}))
+app.use(bp.urlencoded({
+  extended: true
+}))
 
-
+let auth = require('./server-assets/auth/routes')
+app.use(auth.session)
+app.use(auth.router)
 //Code above is always the same ^^
 
 //routes
@@ -25,17 +29,53 @@ var planets = require('./server-assets/routes/planets')
 
 app.use(cats.router)
 app.use(dogs.router)
-app.use(galaxies.router)
+
+app.use('/members/*', (req, res, next) => {
+  if (!req.session.uid) {
+    return res.status(401).send({
+      error: 'please login to continue'
+    })
+  }
+  next()
+})
+
+app.use('/admin/*', (req, res, next) => {
+  /**
+    Admins.findOne({
+        uid: req.session.uid
+      })
+      .then(isAdmin => {
+        if (!isAdmin) {
+          return res.status(401).send({
+            error: 'Insufficient Privledges'
+          })
+        }
+        next()
+      })
+
+    if (!req.session.uid) {
+      return res.status(401).send({
+        error: 'please login to continue'
+      })
+    }
+    next()
+     */
+})
+
+
+app.use('/members/', galaxies.router)
 app.use(stars.router)
 app.use(planets.router)
 
 //Catch all
 
-app.get('*', (req, res, next)=>{
-  res.status(404).send({error: 'No matching routes'})
+app.get('*', (req, res, next) => {
+  res.status(404).send({
+    error: 'No matching routes'
+  })
 })
 
 
-app.listen(port, ()=>{
+app.listen(port, () => {
   console.log('server running on port', port)
 })
---
+++ server-assets/models/user.js
@@ -0,0 +1,33 @@
+var mongoose = require('mongoose')
+var bcrypt = require('bcryptjs')
+var Schema = mongoose.Schema
+var schemaName = 'User'
+const SALT = 12
+
+var schema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  displayName: {
+    type: String,
+    required: true,
+  },
+  hash: {
+    type: String,
+    required: true
+  }
+})
+
+schema.statics.generateHash = function (password) {
+  return bcrypt.hashSync(password, SALT)
+}
+
+schema.methods.validatePassword = function (password) {
+  return bcrypt.compare(password, this.hash)
+}
+
+
+
+module.exports = mongoose.model(schemaName, schema)
